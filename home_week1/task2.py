e750407eecdb86aa0539cea23b19cadf9401b425
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-20 19:00:16 +0300

    t2

---
+++ home_week1/task2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def get_string_compare(str1,str2):
+    str_compare_result = 'undefined'
+   
+    return str_compare_result
+
+
+### Main ###
+
+str1 = input('Input str #1:')
+str2 = input('Input str #2:')
+str_compare = get_string_compare(str1, str2)
+print ('Compare our strings:',str_compare)

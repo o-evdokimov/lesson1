e3955060767b5dda238d6b569eea30a58695be06
Evdokimov Oleg <o.evdokimov@corp.mail.ru>
2019-05-20 17:59:01 +0300

    ok

--- home_week1/task1.py
+++ home_week1/task1.py
@@ -1,2 +1,27 @@
-# #!/usr/local/bin/python3
 #!/usr/bin/python3
+
+
+def get_career(age):
+    prof = 'undefined'
+
+    car = [
+     ['kindergarten child', 0, 5],
+     ['schoolboy', 6, 17],      
+     ['high's school student', 18, 23],   
+     ['job proffesional', 24, 120]
+    ]
+    
+    item = 0
+    for item in range(len(car)):
+        if (age > car[item][1] and age < car[item][2]): 
+       	    prof = car[item][0]
+            break
+   
+    return prof
+
+
+### Main ###
+
+age = input('Input age:')
+career = get_career(int(age))
+print ('The one is',career)
